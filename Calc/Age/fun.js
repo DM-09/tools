b0d0f8a298f97ed4ac222dfacbb5b9effc4a9680
DM <112751504+DM-09@users.noreply.github.com>
2023-01-23 21:04:45 +0900

    Create fun.js

---
+++ Calc/Age/fun.js
@@ -0,0 +1,32 @@
+function AgeCalc(BY, BM, BD) {
+  var now = new Date();
+  var NT = [now.getFullYear(), now.getMonth(), now.getDate()]
+  
+  var age = Number(NT[0] - BY)
+  
+  if (NT[1] >= BM || NT[2] >= BD) {
+    return  age
+  } else {
+    return age - 1
+  }
+};
+
+function KorAgeCalc(BY, BM, BD) { 
+  var now = new Date();
+  var NT = [now.getFullYear(), now.getMonth(), now.getDate()]
+  
+  var age = Number(NT[0] - BY) + 1
+  return  age
+};
+
+function Basic() {
+  var txt = document.querySelector("#input").value;
+  var Day = txt.split('/');
+  document.querySelector("#output").innerHTML = AgeCalc(Day[2], Day[1], Day[2])
+}
+
+function Kor() {
+  var txt = document.querySelector("#input2").value;
+  var Day = txt.split('/');
+  document.querySelector("#output2").innerHTML = KorAgeCalc(Day[2], Day[1], Day[2])
+}
